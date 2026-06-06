192a9b9d724d86a587282a72e167559ede3d5cae
Koukyosyumei <koukyosyumei@hotmail.com>
2026-06-06 15:32:23 -0400

    update

--- dnn/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Minimal fully-connected DNN implemented with NumPy."""
-from .layers import Linear, Conv2D
-from .activations import ReLU, Sigmoid
-from .losses import MSELoss, BCELoss
-from .optimizers import SGD, Adam
-from .network import Sequential
-
-__all__ = ["Linear", "Conv2D", "ReLU", "Sigmoid", "MSELoss", "BCELoss", "SGD", "Adam", "Sequential"]
